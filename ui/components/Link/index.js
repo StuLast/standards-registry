655056fb64dd14f036a3143bd9c02d74d7397dc3
Stu Last <105198429+Stu-Last@users.noreply.github.com>
2022-12-26 15:41:45 +0000

    FIX: rendering of Layout menu items issue

---
+++ ui/components/Link/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Link({ href, text, newWindow = false, className, children }) {
+  return (
+    <a
+      target={newWindow ? '_blank' : '_self'}
+      href={href}
+      rel="noreferrer"
+      className={className}
+    >
+      {text || children || href}
+    </a>
+  );
+}
+export default Link;
